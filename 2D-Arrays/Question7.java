dfcf9ba892127d46746890695ecb911caa2486f1
Ashishjha013 <ashishjha1304@gmail.com>
2025-09-30 04:42:03 +0530

    Updated the logic of code

--- 2D-Arrays/Question7.java
+++ 2D-Arrays/Question7.java
@@ -1,11 +1,36 @@
-// Question No. 2
-// transpose of a matrix
+// Question No. 7
+// LeetCode - 48. Rotate Image
 
 // hint: Use two loops, one for rows and one for columns.
 
 public class Question7 {
   public static void transposeMatrix(int[][] arr) {
     int n = arr.length;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        int temp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = temp;
+      }
+    }
+  }
+
+  public static void reverse(int matrix[][]) {
+    int n = matrix.length;
+
+    for (int row = 0; row < n; row++) {
+      int arr[] = matrix[row];
+      int start = 0, last = n - 1;
+      while (start < last) {
+        int temp = arr[start];
+        arr[start] = arr[last];
+        arr[last] = temp;
+
+        start++;
+        last--;
+      }
+    }
   }
 
   public static void print2DArray(int arr[][]) {
@@ -20,14 +45,14 @@ public class Question7 {
   // main function
   public static void main(String[] args) {
     int[][] arr = {
-        { 1, 2, 3, 4, 5 },
-        { 6, 7, 8, 9, 10 },
-        { 11, 12, 13, 14, 15 },
-        { 16, 17, 18, 19, 20 },
-        { 21, 22, 23, 24, 25 },
+        { 5, 1, 9, 11 },
+        { 2, 4, 8, 10 },
+        { 13, 3, 6, 7 },
+        { 15, 14, 12, 16 },
     };
 
-    // transposeMatrix(arr);
+    transposeMatrix(arr);
+    reverse(arr);
     print2DArray(arr);
   }
 }
