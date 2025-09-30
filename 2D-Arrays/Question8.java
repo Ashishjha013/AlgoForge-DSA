be3b32c65c414af34a8ad968cca1f8c03ec82c58
Ashishjha013 <ashishjha1304@gmail.com>
2025-09-30 16:39:36 +0530

    Add Question8 class for searching in a 2D sorted array

---
+++ 2D-Arrays/Question8.java
@@ -0,0 +1,11 @@
+// Question No. 8
+// Seearch in 2D Sorted Array
+// Leetcode: 74. Search a 2D Matrix
+
+// hint: 
+
+public class Question8 {
+  public static void main(String[] args) {
+    
+  }
+}
