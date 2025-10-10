d226ffe425e688bdd8893aa354de0f6bbee83ed0
Ashishjha013 <ashishjha1304@gmail.com>
2025-10-10 18:50:10 +0530

    Add Question15.java for generating keypad combinations; create Question16.java as a placeholder for future implementation.

---
+++ Recursion-and-Backtracking/Question15.java
@@ -0,0 +1,41 @@
+// Question: 15
+// Get keypad combinations
+
+// hint: Use a recursive function that processes one digit at a time. For each digit, retrieve the corresponding letters from the mapping and append each letter to the combinations generated from the remaining digits. Start with an empty combination and build it up as you process each digit.
+
+import java.util.*;
+
+public class Question15 {
+  static String[] lettersArray = { ",:", "<;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+  public static ArrayList<String> getKeypadCombinations(String str) {
+    if(str.length() == 0) {
+      ArrayList<String> baseAns = new ArrayList<>();;
+      baseAns.add("");
+      return baseAns;
+    }
+
+    String smallerString = str.substring(1);
+    ArrayList<String> smallerAns = getKeypadCombinations(smallerString);
+
+    ArrayList<String> myAns = new ArrayList<>();
+
+    int firstDIgit = str.charAt(0) - '0';
+    String letters = lettersArray[firstDIgit];
+
+    for(int i=0; i<letters.length(); i++) {
+      char letter = letters.charAt(i);
+
+      for(String s : smallerAns) {
+        myAns.add( letter + s);
+      }
+    }
+    return myAns;
+  }
+
+  public static void main(String[] args) {
+    String input = "23";
+    ArrayList<String> combinations = getKeypadCombinations(input);
+    System.out.println(combinations);
+  }
+}
