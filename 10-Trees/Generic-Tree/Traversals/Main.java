b421087daf0b976dc62fa9b22a91a488fa56cfde
Ashish Jha <ashishjha1304@gmail.com>
2026-02-12 12:35:11 +0530

    Refactor Main.java to implement a binary tree structure; replace ArrayList with left and right child references, and update construction and display methods accordingly.

--- 10-Trees/Generic-Tree/Traversals/Main.java
+++ 10-Trees/Generic-Tree/Traversals/Main.java
@@ -1,213 +1,100 @@
-// Generic Tree Traversals
-
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Stack;
 
+// TreeNode class representing a node in a binary tree with
+// integer data and references to left and right children
 class TreeNode {
   int data;
-  ArrayList<TreeNode> children;
+  TreeNode left;
+  TreeNode right;
 
   public TreeNode(int data) {
     this.data = data;
-    children = new ArrayList<>();
+  }
+}
+
+// Pair class to hold a TreeNode and its state during
+// construction and traversal processes
+class Pair {
+  TreeNode node;
+  int state;
+
+  public Pair(TreeNode node, int state) {
+    this.node = node;
+    this.state = state;
   }
 }
 
 class Main {
-  public static TreeNode constructTree(int[] dataArray) {
-    Stack<TreeNode> st = new Stack<>();
+  // Constructing a binary tree from an array
+  public static TreeNode construction(Integer[] arr) {
+    Stack<Pair> st = new Stack<>();
     TreeNode root = null;
 
-    for (int i = 0; i < dataArray.length; i++) {
-      if (dataArray[i] == -1) {
-        st.pop();
-      } else {
+    for (int i = 0; i < arr.length; i++) {
 
-        TreeNode newNode = new TreeNode(dataArray[i]);
+      Integer ele = arr[i]; // optional readability improvement
+
+      if (ele == null) {
+
         if (st.size() == 0) {
-          root = newNode;
+          return null;
+
+        } else if (st.peek().state == 0) {
+          st.peek().state++;
+
         } else {
-          st.peek().children.add(newNode);
+          st.pop();
         }
 
-        st.push(newNode);
+      } else {
+
+        TreeNode newNode = new TreeNode(ele);
+
+        if (st.size() == 0) {
+          root = newNode;
+
+        } else if (st.peek().state == 0) {
+          st.peek().node.left = newNode;
+          st.peek().state++;
+
+        } else {
+          st.peek().node.right = newNode;
+          st.pop();
+        }
+
+        st.push(new Pair(newNode, 0));
       }
     }
 
     return root;
   }
 
-  // Traversing tree recursively ====================
-  public static void traverse(TreeNode root) {
-    System.out.println("Preorder -> " + root.data);
+  // Displaying a binary tree
+  public static void display(TreeNode root) {
+    if (root == null)
+      return;
 
-    for (TreeNode child : root.children) {
-      System.out.println("Traversing on edge from " + root.data + " -> " + child.data);
+    String nodeStructure = (root.left == null ? "." : root.left.data)
+        + " <- " + root.data + " -> "
+        + (root.right == null ? "." : root.right.data);
 
-      traverse(child);
+    System.out.println(nodeStructure);
 
-      System.out.println("Traversing on edge from " + child.data + " -> " + root.data);
-    }
-
-    System.out.println("Postorder -> " + root.data);
+    display(root.left);
+    display(root.right);
   }
 
-  // Level order traversal
-  public static void levelOrderTraversal(TreeNode root) {
-    LinkedList<TreeNode> que = new LinkedList<>();
-    que.addLast(root);
-
-    while (que.size() > 0) {
-      // remove node
-      TreeNode frontNode = que.removeFirst();
-
-      // print node data
-      System.out.print(frontNode.data + ", ");
-
-      // add node.children
-      for (TreeNode child : frontNode.children) {
-        que.addLast(child);
-      }
-    }
-  }
-
-  // Level order linewise traversal ====================================
-  public static void levelOrderLineWise(TreeNode root) {
-    LinkedList<TreeNode> mainQ = new LinkedList<>();
-    LinkedList<TreeNode> childQ = new LinkedList<>();
-    int level = 1;
-
-    mainQ.addLast(root);
-
-    while (mainQ.size() > 0) {
-      // remove everything from mainQ
-      TreeNode parent = mainQ.removeFirst();
-
-      System.out.println("Level " + level + " -> ");
-      // then print
-      System.out.print(parent.data + ",");
-
-      // add children in childQ
-      for (TreeNode child : parent.children) {
-        childQ.add(child);
-      }
-
-      if (mainQ.size() == 0) {
-        level++;
-        System.out.println();
-        mainQ = childQ; // every parent is removed, we start with childQ again
-        childQ = new LinkedList<>(); // emptying childQ
-      }
-    }
-  }
-
-  // Using marker =====================
-  public static void levelOrderLineWise2(TreeNode root) {
-    LinkedList<TreeNode> que = new LinkedList<>();
-    que.add(root);
-    que.add(null); // marker marking end of level
-
-    int level = 1;
-
-    while (que.size() > 0) {
-      TreeNode frontNode = que.removeFirst();
-
-      if (frontNode != null) {
-        System.out.println("Level " + level + " => ");
-        System.out.print(frontNode.data + ", ");
-
-        for (TreeNode child : frontNode.children) {
-          que.addLast(child);
-        }
-
-      } else {
-        level++;
-        System.out.println();
-
-        if (que.size() > 0) {
-          que.addLast(null); // marker for next end of level
-        }
-      }
-    }
-  }
-
-  // Method 3
-  public static void levelOrderLineWise3(TreeNode root) {
-    LinkedList<TreeNode> que = new LinkedList<>();
-    int level = 1;
-    que.add(root);
-
-    while (que.size() > 0) {
-      // number of nodes at current level
-      int currentLevelNodes = que.size();
-
-      System.out.print("Level " + level + " => ");
-
-      // process all nodes of current level
-      while (currentLevelNodes > 0) {
-        TreeNode frontNode = que.removeFirst();
-
-        System.out.print(frontNode.data + ", ");
-
-        for (TreeNode child : frontNode.children) {
-          que.addLast(child);
-        }
-
-        currentLevelNodes--;
-      }
-
-      level++;
-      System.out.println();
-    }
-  }
-
-  // Zig zag traversal
-  public static void zigZagTraversal(TreeNode root) {
-    Stack<TreeNode> mainSt = new Stack<>();
-    Stack<TreeNode> childSt = new Stack<>();
-    int level = 1;
-    mainSt.add(root);
-
-    while (mainSt.size() > 0) {
-      TreeNode topNode = mainSt.pop();
-
-      System.out.print(topNode.data + ", ");
-
-      if (level % 2 == 1) { // odd levels, insert from left to right
-
-        for (int i = 0; i < topNode.children.size(); i++) {
-          TreeNode child = topNode.children.get(i);
-          childSt.add(child);
-        }
-
-      } else { // insert children from right to left
-
-        for (int i = topNode.children.size() - 1; i >= 0; i--) {
-          TreeNode child = topNode.children.get(i);
-          childSt.add(child);
-        }
-      }
-
-      if (mainSt.size() == 0) {
-        level++;
-        System.out.println();
-        mainSt = childSt;
-        childSt = new Stack<>();
-      }
-    }
-  }
-
+  // Main function
   public static void main(String[] args) {
-    // int[] dataArray1 = {10,20,50,-1,60,-1,-1,30,-1,40,80,-1,90,-1,100,-1,-1,-1};
-    int[] dataArray2 = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, -1, 40, 80, -1, 90, 110, -1, 120, -1, -1, 100, -1, -1,
-        -1 };
+    Integer[] arr = {
+        10, 20, 40, null, 60, null, null,
+        50, 70, null, null, null,
+        30, 80, 90, null, null, 100, null, null, null
+    };
 
-    // TreeNode root = constructTree(dataArray1);
-    TreeNode root2 = constructTree(dataArray2);
+    TreeNode root = construction(arr);
 
-    // traverse(root);
-    levelOrderLineWise3(root2);
-    // zigZagTraversal(root2);
+    System.out.println("Tree Structure:");
+    display(root);
   }
 }
